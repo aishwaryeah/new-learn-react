d648d687c05ec35f54be58bb394d8b41aa6c349d
aishwaryeah <73528153+aishwaryeah@users.noreply.github.com>
2024-03-28 00:03:36 +0530

    class 03

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
+const Title = () => (
+  <h1 className="heading" tabIndex="5">
+    Hello World!
+  </h1>
+);
+
+const HeadingCompnent = () => (
+  <div id="container">
+    <Title />
+    <h1>This is a Functional Component</h1>
+  </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingCompnent />);
