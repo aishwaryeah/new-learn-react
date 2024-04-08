85cf058fca1fd4193f9e9168678e91b6b3edf5f9
aishwaryeah <73528153+aishwaryeah@users.noreply.github.com>
2024-04-08 22:03:43 +0530

    class 09 ans class 10

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -26,6 +27,14 @@ const Body = () => {
     );
   };
 
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Oops! Looks like you're offline. Please check your internet connection
+      </h1>
+    );
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnLogin, setBtnLogin] = useState("Login");
-
+  const onlineStatus = useOnlineStatus();
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +13,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status : {onlineStatus ? "✅" : "✖️"}</li>
           <li>
             <Link to="/">Home</Link>
           </li>
